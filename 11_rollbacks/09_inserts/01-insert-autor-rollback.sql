39d174ff863be65d3254d137e139c20672acd52b
Nicolas_Ruiz <nruizsastoque@gmail.com>
2026-03-25 19:17:49 -0500

    qa  llega a HU-06-main

---
+++ 11_rollbacks/09_inserts/01-insert-autor-rollback.sql
@@ -0,0 +1,13 @@
+DELETE FROM autor
+WHERE nombre IN (
+  'Gabriel García Márquez',
+  'Mario Vargas Llosa',
+  'Julio Cortázar',
+  'Isabel Allende',
+  'Jorge Luis Borges',
+  'Pablo Neruda',
+  'Octavio Paz',
+  'Carlos Fuentes',
+  'Laura Restrepo',
+  'Fernando Vallejo'
+);
